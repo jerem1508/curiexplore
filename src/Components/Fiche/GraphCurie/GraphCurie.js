b266af6ca689c9bfd43f082d6f9e27d9745c485a
Tanguy D'auria <tanguy.d-auria@epitech.eu>
2019-06-14 14:46:08 +0100

    If axios.get() was previously done, retrieving data from allData

--- src/Components/Fiche/GraphCurie/GraphCurie.js
+++ src/Components/Fiche/GraphCurie/GraphCurie.js
@@ -15,12 +15,12 @@ class GraphCurie extends Component {
   constructor(props) {
     super(props);
     this.country = null;
-    this.countryList = [this.props.countryCode];
+    this.countryList = [this.props.countryCode.toUpperCase()];
     this.graphIndex = 0;
+    this.allData = [];
     this.indic = 0;
     this.state = {
       isMissing: true,
-      allData: [],
       filterData: null,
     };
     this.getGraphValues = this.getGraphValues.bind(this);
@@ -32,7 +32,7 @@ class GraphCurie extends Component {
   componentDidMount() {
     let i = 0;
     // On vérifie si le code iso 3 est valide
-    this.country = this.props.countryCode;
+    this.country = this.props.countryCode.toUpperCase();
     for (i = 0; i < isoList.length; i += 1) {
       if (isoList[i]['alpha-3'] === this.country) {
         this.setState({ isMissing: false });
@@ -79,22 +79,43 @@ class GraphCurie extends Component {
     // On vérifie si la data est dispo afin d'éviter les requêtes inutiles
     // todo: on boucle sur chaque element et si pas dispo on met null, et on le remplace après (il faut [FRA, code, requete where])
     for (let i = 0; i < this.countryList.length; i += 1) {
-      for (let j = 0; j < this.state.allData.length; j += 1) {
-        if ((this.state.allData[j][0] === this.countryList[i]) && (this.state.allData[j][1] === params[label][0].unit[index].code)) {
-          tempData[i] = this.state.allData[2];
+      for (let j = 0; j < this.allData.length; j += 1) {
+        if ((this.allData[j][0] === this.countryList[i]) && (this.allData[j][1] === params[label][indic].unit[index].code)) {
+          tempData[i] = this.allData[2];
         }
       }
     }
 
     const results = [];
+    const tempCtry = [];
     for (let i = 0; i < tempData.length; i += 1) {
       if (tempData[i] === null) {
         // On met l'appel de fonction à faire dans results (pour await toutes les réponses avant de continuer)
+        tempCtry.push(this.countryList[i]);
         results.push(this.getData(i, label, index, indic));
+      } else {
+        results.push('');
       }
     }
-    // On effectue toutes les requêtes nécessaires
+    // On effectue toutes les requêtes nécssaires
     tempData = await Promise.all(results);
+    for (let i = 0; i < results.length; i += 1) {
+      if (tempData[i] === '') {
+        for (let j = 0; j < this.allData.length; j += 1) {
+          if ((this.allData[j][0] === this.countryList[i]) && (this.allData[j][1] === params[label][indic].unit[index].code)) {
+            tempData[i] = this.allData[j][2];
+          }
+        }
+      } else {
+        const tmpLbl = [];
+        tmpLbl.push(this.countryList[i]);
+        tmpLbl.push(params[label][indic].unit[index].code);
+        tmpLbl.push(tempData[i]);
+        this.allData.push(tmpLbl);
+      }
+    }
+    // alert(this.allData[0]);
+    // alert(tempData[0]);
     this.setState({ filterData: tempData });
   }
 
@@ -132,11 +153,11 @@ class GraphCurie extends Component {
               <div>
                 <GraphHeader handleIndic={this.handleIndic} value={this.state.label} />
                 <Row>
-                  <Col>
-                    <p>Je suis le pays</p>
+                  <Col sm={11}>
+                    <p>Dans la légende</p>
                   </Col>
-                  <Col>
-                    <i className="fal fa-info-circle" />
+                  <Col sm={1}>
+                    <i className="fas fa-info-circle" />
                   </Col>
                 </Row>
                 {this.state.filterData ? <HighChartsBar style={{ backgroundColor: 'white' }} data={this.state.filterData} /> : <div>Loading</div>}
@@ -160,7 +181,6 @@ export default GraphCurie;
 GraphCurie.propTypes = {
   countryCode: PropTypes.string.isRequired,
   graphType: PropTypes.string.isRequired,
-  countryName: PropTypes.string.isRequired,
   // language: PropTypes.string.isRequired,
   // switchLanguage: PropTypes.func.isRequired,
 };
