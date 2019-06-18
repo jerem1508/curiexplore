25309488108cf0f300598815e2fb081ccd7c9a8a
miarkt <rakoto.mk@gmail.com>
2019-06-18 20:34:52 +0200

    création liste

--- src/Components/Homepage/CountriesList/CountriesList.js
+++ src/Components/Homepage/CountriesList/CountriesList.js
@@ -10,6 +10,9 @@ import messagesEn from './translations/en.json';
 /* SCSS */
 import classes from './CountriesList.scss';
 
+/* Data */
+import countries from './countriesList.json';
+
 const messages = {
   fr: messagesFr,
   en: messagesEn,
@@ -17,64 +20,93 @@ const messages = {
 
 class CountriesList extends Component {
   state = {
-    data: [],
+    data: countries,
   };
 
-  componentDidMount() {
-    this.getData();
-  }
+  // componentDidMount() {
+  //   this.getData();
+  // }
+  //
+  // getData = () => {
+  //   Axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
+  //     this.setState({ data: response.data });
+  //   }).catch((e) => {
+  //     console.log(e);
+  //   });
+  // }
 
-  getData = () => {
-    Axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
-      this.setState({ data: response.data });
-    }).catch((e) => {
-      console.log(e);
-    });
+  createColumn = (column) => {
+    const col = [];
+    let letter = '';
+    for (let i = 0; i < column.length; i += 1) {
+      let title = null;
+      if (letter !== column[i]) {
+        letter = column[i];
+        // + affichage du titre
+        title = <dt>{column[i]}</dt>;
+      }
+
+      const countryName = [];
+      for (let j = 0; j < this.state.data.length; j += 1) {
+        if (this.state.data[j].Pays.substr(0, 1) === letter) {
+          const a = (
+            <li className={classes.country}>
+              <a href={`/fiche/${this.state.data[j].ISO_alpha3}`}>
+                {this.state.data[j].Pays}
+              </a>
+            </li>
+          );
+          countryName.push(a);
+        }
+      }
+      col.push(<dl>{title}<ul>{countryName}</ul></dl>);
+    }
+    return col;
   }
 
   render() {
-    let countryName = null;
+    if (this.state.data.length === 0) {
+      return null;
+    }
 
     const letterCol1 = ['A', 'B', 'C', 'D'];
     const letterCol2 = ['E', 'F', 'G', 'H', 'I', 'J', 'K'];
     const letterCol3 = ['L', 'M', 'N', 'O', 'P', 'Q', 'R'];
     const letterCol4 = ['S', 'T', 'U', 'V', 'Y', 'Z'];
 
-    if (this.state.data.length > 0) {
-      countryName = this.state.data.map((country) => {
-        if (country.name.substr(0, 1) === letterCol1.map(letter => letter)) {
-          return (
-            <li className={classes.country}>
-              {country.translations.fr}
-            </li>
-          );
-        }
-        return null;
-      });
-    }
-
-    // const content = this.state.data.map(country => (
-    //   <div className="container">
-    //     <div>
-    //       {country.translations.fr}
-    //     </div>
-    //   </div>
-    // ));
+    const col1 = this.createColumn(letterCol1);
+    const col2 = this.createColumn(letterCol2);
+    const col3 = this.createColumn(letterCol3);
+    const col4 = this.createColumn(letterCol4);
 
     return (
       <div className="container-fluid">
         <div className="container">
-          <div className="col-3">
-            <dl>
-              <dt>
-                A
-              </dt>
-              {countryName}
-            </dl>
+          <div className="row">
+            Recherchez un pays dans la liste suivante
           </div>
-          <div className="col-3"></div>
-          <div className="col-3"></div>
-          <div className="col-3"></div>
+          <div className={`row ${classes.countryList}`}>
+            <div className="col-3">
+              {
+                col1.map(letterList => letterList)
+              }
+            </div>
+            <div className="col-3">
+              {
+                col2.map(letterList => letterList)
+              }
+            </div>
+            <div className="col-3">
+              {
+                col3.map(letterList => letterList)
+              }
+            </div>
+            <div className="col-3">
+              {
+                col4.map(letterList => letterList)
+              }
+            </div>
+          </div>
         </div>
       </div>
     );
